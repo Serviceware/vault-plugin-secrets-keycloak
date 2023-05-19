e7101af534889bcf2964a3b279d514472ecc5bd2
Max Fröhlich <max.froehlich@serviceware.de>
2023-05-19 16:16:03 +0200

    chore: replace testing setup with terraform

--- backend_test.go
+++ backend_test.go
@@ -2,12 +2,13 @@ package keycloak
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 
-	"github.com/Nerzal/gocloak/v13"
 	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
 	logicaltest "github.com/hashicorp/vault/helper/testhelpers/logical"
 	"github.com/hashicorp/vault/sdk/logical"
 	"github.com/mitchellh/mapstructure"
@@ -15,86 +16,168 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
+// keycloak admin user/pass
+// admin/admin
+const (
+	keycloakUsername = "admin"
+	keycloakPassword = "admin"
+)
+
 // inspired by https://github.com/hashicorp/vault/blob/main/builtin/logical/rabbitmq/backend_test.go
 
-func prepareKeycloakTestContainer(t *testing.T) (func(), string, string, string, string) {
+func prepareLegacyKeycloakTestContainer(t *testing.T) (func(), string, string, string, string) {
 
 	t.Helper()
-	keycloakUsername := "admin"
-	keycloakPassword := "admin"
-	client_id := "vault"
-	client_secret := "vault"
+	client := "vault"
 	realm := "master"
 
 	ctx := context.Background()
-	req := testcontainers.ContainerRequest{
-		Image:        "jboss/keycloak:16.1.1",
-		ExposedPorts: []string{"8080/tcp"},
-		WaitingFor:   wait.ForHTTP("/").WithMethod("GET").WithPort(nat.Port("8080")).WithStartupTimeout(time.Second * 90),
-		Env: map[string]string{
-			"KEYCLOAK_USER":     keycloakUsername,
-			"KEYCLOAK_PASSWORD": keycloakPassword,
-			"DB_VENDOR":         "H2",
-		},
-	}
+	networkName, cleanupNetwork := createTestingNetwork(t, ctx)
 
-	keycloakC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
+	keycloakC, cleanupKeycloak := startLegacyKeycloak(t, ctx, networkName)
+
 	ip, err := keycloakC.Host(ctx)
 	if err != nil {
-		t.Error(err)
+		t.Fatalf("Failed to get keycloak container ip: %s", err)
 	}
 	port, err := keycloakC.MappedPort(ctx, "8080")
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("Failed to get keycloak container port: %s", err)
 	}
 	serverUrl := fmt.Sprintf("http://%s:%s/auth", ip, port.Port())
-	keycloakCLient := gocloak.NewClient(serverUrl)
 
-	loginToken, err := keycloakCLient.Login(ctx, "admin-cli", "", "master", keycloakUsername, keycloakPassword)
-
-	if err != nil {
-		t.Fatal(err)
+	applyTerraform(t, ctx, networkName, `
+	terraform {
+	   required_providers {
+		 keycloak = {
+		   source = "mrparkers/keycloak"
+		   version = "4.2.0"
+		 }
+	   }
+	 }
+	 
+	 provider "keycloak" {
+		 # set by environment variables
+		 client_id     = "admin-cli"
+		 username      = "admin"
+		 password      = "admin"
+		 url           = "http://keycloak:8080"
+		 base_path = "/auth"
+	 }
+	 variable "client" {
+	   type = string
+	 }
+	 data "keycloak_realm" "realm" {
+		 realm = "master"
+	 }
+	 data "keycloak_role" "admin" {
+		realm_id = data.keycloak_realm.realm.id
+		name     = "admin"
+	 }
+	  
+	 resource "keycloak_openid_client" "openid_client" {
+		realm_id            =  data.keycloak_realm.realm.id
+		client_id           = var.client
+		client_secret 		= var.client
+		enabled             = true
+		access_type         = "CONFIDENTIAL"
+		service_accounts_enabled = true	
 	}
-	serviceAccountsEnabled := true
-
-	createClientResponse, err := keycloakCLient.CreateClient(ctx, loginToken.AccessToken, realm, gocloak.Client{
-		ClientID:               &client_id,
-		Secret:                 &client_secret,
-		ServiceAccountsEnabled: &serviceAccountsEnabled,
+	resource "keycloak_openid_client_service_account_realm_role" "client_service_account_role" {
+		realm_id                = data.keycloak_realm.realm.id
+		service_account_user_id = keycloak_openid_client.openid_client.service_account_user_id
+		role                    = data.keycloak_role.admin.name
+	}
+   
+   `, map[string]interface{}{
+		"client": client,
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	clientServiceAccount, err := keycloakCLient.GetClientServiceAccount(ctx, loginToken.AccessToken, realm, createClientResponse)
-	if err != nil {
-		t.Fatal(err)
-	}
-	adminRole, err := keycloakCLient.GetRealmRole(ctx, loginToken.AccessToken, realm, "admin")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = keycloakCLient.AddRealmRoleToUser(ctx, loginToken.AccessToken, realm, *clientServiceAccount.ID, []gocloak.Role{*adminRole})
-
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	//serverUrl := "http://localhost:8080"
 	return func() {
-		keycloakC.Terminate(ctx)
-	}, serverUrl, realm, client_id, client_secret
+		cleanupKeycloak()
+		cleanupNetwork()
+	}, serverUrl, realm, client, client
 }
 
-func TestBackend_basic(t *testing.T) {
+func prepareKeycloakTestContainer(t *testing.T, version string) (func(), string, string, string, string) {
+
+	t.Helper()
+	client := "vault"
+	realm := "master"
+
+	ctx := context.Background()
+	networkName, cleanupNetwork := createTestingNetwork(t, ctx)
+
+	keycloakC, cleanupKeycloak := startKeycloakWithVersion(t, ctx, networkName, version)
+
+	ip, err := keycloakC.Host(ctx)
+	if err != nil {
+		t.Fatalf("Failed to get keycloak container ip: %s", err)
+	}
+	port, err := keycloakC.MappedPort(ctx, "8080")
+	if err != nil {
+		t.Fatalf("Failed to get keycloak container port: %s", err)
+	}
+	serverUrl := fmt.Sprintf("http://%s:%s", ip, port.Port())
+
+	applyTerraform(t, ctx, networkName, `
+	terraform {
+	   required_providers {
+		 keycloak = {
+		   source = "mrparkers/keycloak"
+		   version = "4.2.0"
+		 }
+	   }
+	 }
+	 
+	 provider "keycloak" {
+		 # set by environment variables
+		 client_id     = "admin-cli"
+		 username      = "admin"
+		 password      = "admin"
+		 url           = "http://keycloak:8080"
+	
+	 }
+	 variable "client" {
+	   type = string
+	 }
+	 data "keycloak_realm" "realm" {
+		 realm = "master"
+	 }
+	 data "keycloak_role" "admin" {
+		realm_id = data.keycloak_realm.realm.id
+		name     = "admin"
+	 }
+	  
+	 resource "keycloak_openid_client" "openid_client" {
+		realm_id            =  data.keycloak_realm.realm.id
+		client_id           = var.client
+		client_secret 		= var.client
+		enabled             = true
+		access_type         = "CONFIDENTIAL"
+		service_accounts_enabled = true	
+	}
+	resource "keycloak_openid_client_service_account_realm_role" "client_service_account_role" {
+		realm_id                = data.keycloak_realm.realm.id
+		service_account_user_id = keycloak_openid_client.openid_client.service_account_user_id
+		role                    = data.keycloak_role.admin.name
+	}
+   
+   `, map[string]interface{}{
+		"client": client,
+	})
+
+	//serverUrl := "http://localhost:8080"
+	return func() {
+		cleanupKeycloak()
+		cleanupNetwork()
+	}, serverUrl, realm, client, client
+}
+func TestBackend_basic_on_legacy(t *testing.T) {
 	b, _ := Factory(context.Background(), logical.TestBackendConfig())
 
-	cleanup, server_url, realm, client_id, client_secret := prepareKeycloakTestContainer(t)
+	cleanup, server_url, realm, client_id, client_secret := prepareLegacyKeycloakTestContainer(t)
 	defer cleanup()
 
 	logicaltest.Test(t, logicaltest.TestCase{
@@ -107,6 +190,29 @@ func TestBackend_basic(t *testing.T) {
 			testAccStepConfigDelete(t),
 		},
 	})
+}
+func TestBackendWithKeycloak(t *testing.T) {
+	versions := []string{"21.1.1", "20.0.5", "19.0.3"}
+
+	for _, version := range versions {
+		t.Run(fmt.Sprintf("Keycloak %s", version), func(t *testing.T) {
+			b, _ := Factory(context.Background(), logical.TestBackendConfig())
+
+			cleanup, serverURL, realm, clientID, clientSecret := prepareKeycloakTestContainer(t, version)
+			defer cleanup()
+
+			logicaltest.Test(t, logicaltest.TestCase{
+				PreCheck:       testAccPreCheckFunc(t, serverURL),
+				LogicalBackend: b,
+				Steps: []logicaltest.TestStep{
+					testAccStepConfig(t, serverURL, realm, clientID, clientSecret),
+					testAccStepReadConfig(t, serverURL, realm, clientID, clientSecret),
+					testAccStepReadSecret(t, clientID, clientSecret),
+					testAccStepConfigDelete(t),
+				},
+			})
+		})
+	}
 }
 func testAccPreCheckFunc(t *testing.T, uri string) func() {
 	return func() {
@@ -196,4 +302,142 @@ func testAccStepReadSecret(t *testing.T, clientId string, expectedClientSecret s
 			return nil
 		},
 	}
+}
+
+func startKeycloakWithVersion(t *testing.T, ctx context.Context, networkName string, keycloakVersion string) (testcontainers.Container, func()) {
+	keycloakContainer, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+
+			Image:        fmt.Sprintf("quay.io/keycloak/keycloak:%s", keycloakVersion),
+			ExposedPorts: []string{"8080/tcp"},
+			WaitingFor:   wait.ForHTTP("/").WithMethod("GET").WithPort(nat.Port("8080")).WithStartupTimeout(time.Second * 90),
+			Env: map[string]string{
+				"KEYCLOAK_ADMIN":          "admin",
+				"KEYCLOAK_ADMIN_PASSWORD": "admin",
+			},
+			Cmd: []string{"start-dev"},
+			Networks: []string{
+				networkName,
+			},
+			NetworkAliases: map[string][]string{
+				networkName: {"keycloak"},
+			},
+		},
+		Started: true,
+	})
+	if err != nil {
+		t.Fatalf("Failed to start keycloak container: %s", err)
+	}
+	return keycloakContainer, func() {
+		if err := keycloakContainer.Terminate(ctx); err != nil {
+			t.Errorf("failed to terminate container: %s", err.Error())
+		}
+	}
+}
+
+func startLegacyKeycloak(t *testing.T, ctx context.Context, networkName string) (testcontainers.Container, func()) {
+	keycloakC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "jboss/keycloak:16.1.1",
+			ExposedPorts: []string{"8080/tcp"},
+			WaitingFor:   wait.ForHTTP("/").WithMethod("GET").WithPort(nat.Port("8080")).WithStartupTimeout(time.Second * 90),
+			Env: map[string]string{
+				"KEYCLOAK_USER":     keycloakUsername,
+				"KEYCLOAK_PASSWORD": keycloakPassword,
+				"DB_VENDOR":         "H2",
+			},
+			Networks: []string{
+				networkName,
+			},
+			NetworkAliases: map[string][]string{
+				networkName: {"keycloak"},
+			},
+		},
+		Started: true,
+	})
+	if err != nil {
+		t.Fatalf("Failed to start keycloak container: %s", err)
+	}
+	return keycloakC, func() {
+		if err := keycloakC.Terminate(ctx); err != nil {
+			t.Errorf("failed to terminate container: %s", err.Error())
+		}
+	}
+
+}
+func createTestingNetwork(t *testing.T, ctx context.Context) (string, func()) {
+
+	t.Helper()
+
+	// random network name
+	networkName := fmt.Sprintf("test-network-%s", uuid.New().String())
+
+	newNetwork, err := testcontainers.GenericNetwork(ctx, testcontainers.GenericNetworkRequest{
+		NetworkRequest: testcontainers.NetworkRequest{
+			Name:           networkName,
+			CheckDuplicate: true,
+		},
+	})
+	if err != nil {
+		t.Fatalf("Failed to create network: %s", err)
+	}
+	return networkName, func() {
+		if err := newNetwork.Remove(ctx); err != nil {
+			t.Errorf("failed to remove network: %s", err.Error())
+		}
+	}
+}
+
+func applyTerraform(t *testing.T, ctx context.Context, networkName string, terraformContent string, vars map[string]interface{}) {
+
+	t.Helper()
+
+	content := []byte(terraformContent)
+
+	req := testcontainers.ContainerRequest{
+		Image:      "hashicorp/terraform:latest",
+		WaitingFor: wait.ForLog("Apply complete!").WithStartupTimeout(time.Second * 30),
+
+		Entrypoint: []string{"sh", "-c", "cd /opt && terraform init -backend=false && terraform apply -auto-approve"},
+		Networks: []string{
+			networkName,
+		},
+	}
+
+	terraformC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          false,
+	})
+	defer func() {
+		if err := terraformC.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	if err != nil {
+		t.Errorf("Failed to start terraform container: %s", err)
+	}
+
+	err = terraformC.CopyToContainer(ctx, content, "/opt/main.tf", 777)
+
+	if err != nil {
+		t.Fatalf("Failed to copy file: %s", err)
+	}
+	// copy vars to config.auto.tfvars.json
+	if vars != nil {
+		varsContent, err := json.Marshal(vars)
+		if err != nil {
+			t.Fatalf("Failed to marshal vars: %s", err)
+		}
+		err = terraformC.CopyToContainer(ctx, varsContent, "/opt/config.auto.tfvars.json", 777)
+		if err != nil {
+			t.Fatalf("Failed to copy vars: %s", err)
+		}
+	}
+
+	err = terraformC.Start(ctx)
+	if err != nil {
+		t.Fatalf("Failed to start container: %s", err)
+	}
+
 }
