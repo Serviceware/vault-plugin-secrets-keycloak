1273f508c75961e2a681f731eca3857782fd1d44
hkohlsaat <hkohlsaat@gmail.com>
2025-09-18 22:22:24 +0200

    improving documentation of keycloakservice package

--- keycloakservice/interface.go
+++ keycloakservice/interface.go
@@ -1,3 +1,5 @@
+// Package keycloaksearvice adapts keycloak functionality.
+// Its core is the [KeycloakService] and its implementations.
 package keycloakservice
 
 import (
@@ -13,17 +15,20 @@ type ConnectionConfig struct {
 	ClientSecret string
 }
 type WellKnownOpenidConfiguration struct {
-	Issuer string `json:"issuer"` //Enough for now
+	Issuer string `json:"issuer"`
 }
 
+// KeycloakService describes the relevant subset of keycloak functionality for providing secrets to vault.
 type KeycloakService interface {
-	// defin the same methods as gocloak.GoCloak (only LoginClient, GetClients, GetClientSecret are used)
-	// I know looks not clean when I leak gocloak types, but I don't want to reimplement all the structs
+	// Defining the same methods as gocloak.GoCloak.
+	// I know it looks not clean when I leak gocloak types, but I don't want to reimplement all the structs
 	LoginClient(ctx context.Context, clientID string, clientSecret string, realm string) (*gocloak.JWT, error)
 	GetClients(ctx context.Context, token string, realm string, params gocloak.GetClientsParams) ([]*gocloak.Client, error)
 	GetClientSecret(ctx context.Context, token string, realm string, clientID string) (*gocloak.CredentialRepresentation, error)
 	GetWellKnownOpenidConfiguration(ctx context.Context, realm string) (*WellKnownOpenidConfiguration, error)
 }
+
+// KeycloakServiceFactory abstracts the creation of a new [KeycloakService] instance.
 type KeycloakServiceFactory interface {
 	NewClient(ctx context.Context, connConfig ConnectionConfig) (KeycloakService, error)
 }
