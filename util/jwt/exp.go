e106a12339f32109fee0552ca35922425e2af623
hkohlsaat <hkohlsaat@gmail.com>
2025-11-06 18:01:50 +0100

    avoiding memory allocation for jwt decomposition

--- util/jwt/exp.go
+++ util/jwt/exp.go
@@ -11,12 +11,12 @@ import (
 // ExpirationTime is a simple helper function that extracts the expiration
 // time claim from jwt and retuns it as [time.Time].
 func ExpirationTime(jwt string) (time.Time, error) {
-	parts := strings.Split(jwt, ".")
-	if len(parts) != 3 {
-		return time.Time{}, fmt.Errorf("jwt has != 3 parts")
+	_, payloadPart, _, err := parts(jwt)
+	if err != nil {
+		return time.Time{}, err
 	}
 
-	payload, err := base64.RawURLEncoding.DecodeString(parts[1])
+	payload, err := base64.RawURLEncoding.DecodeString(payloadPart)
 	if err != nil {
 		return time.Time{}, err
 	}
@@ -31,6 +31,16 @@ func ExpirationTime(jwt string) (time.Time, error) {
 	}
 
 	return time.Unix(*parsedPayload.Exp, 0), nil
+}
+
+func parts(jwt string) (string, string, string, error) {
+	header, payloadAndSignature, hasHeader := strings.Cut(jwt, ".")
+	payload, signature, hasPayload := strings.Cut(payloadAndSignature, ".")
+	dividesSignature := strings.Contains(signature, ".")
+	if !hasHeader || !hasPayload || dividesSignature {
+		return "", "", "", fmt.Errorf("jwt has != 3 parts")
+	}
+	return header, payload, signature, nil
 }
 
 // IsValidIn checks whether jwt is still valid in the future at
